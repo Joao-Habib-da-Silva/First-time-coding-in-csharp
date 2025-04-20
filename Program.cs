39ab48b04c7f48eac22dcc0019f419943074f653
Joao Habib da Silva <contajh544@gmail.com>
2025-04-20 20:14:00 -0300

    first commit eith coding in csharp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Program
+{
+ static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world");
+        float number = 0.1f + 0.2f;
+        Console.WriteLine(number == 0.3f);
+        decimal Number = 0.1m + 0.2m;
+        Console.WriteLine(Number == 0.3m);
+    }
+}
